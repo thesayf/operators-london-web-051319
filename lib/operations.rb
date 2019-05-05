95ae9ad0c36e2bf25f7c66e17c48db94e59ffd60
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-05 00:14:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
 
-end
+   if speed < 40 || speed > 60
+    return true
+  else
+    return false
+  end
+end	end
 
-
-
-def not_safe?(speed)
-	
-end
-	
+ def not_safe?(speed)	def not_safe?(speed)
+		  speed < 40 || speed > 60 ? true : false
+end	end
 
 
